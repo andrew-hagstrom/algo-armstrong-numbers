f3d51a874dfd35b83affec71ab75868b7b7d602d
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2023-10-09 11:28:43 -0500

    my first commit

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,15 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    armstrong=[]
+    sum = 0
+    for num in range(0, numbers[-1]+1):
+        num_str = str(num)
+        l = len(num_str)
+        for digit in num_str: 
+            sum +=int(digit)**l
+        if sum == num:
+            armstrong.append(num)
+        sum=0
+    return(armstrong)
+find_armstrong_numbers([1000])
+
